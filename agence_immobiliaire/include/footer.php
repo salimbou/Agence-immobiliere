1c096d0c6d87ace971354eccda930e184750e63b
salimbou <salimboualita95@gmail.com>
2024-05-03 10:01:55 +0100

    Agence Immobiliere

---
+++ agence_immobiliaire/include/footer.php
@@ -0,0 +1,67 @@
+<footer class="full-row bg-secondary p-0">
+            <div class="container">
+                <div  class="row">
+                    <div class="col-lg-12">
+                        <div class="divider py-40">
+                            <div class="row">
+                                <div class="col-md-12 col-lg-4">
+                                    <div class="footer-widget mb-4">
+                                        <div class="footer-logo mb-4"> <a href="#"><img class="logo-bottom" src="images/logo/restatelg_white.png" alt="image"></a> </div>
+                                        <p class="pb-20 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, voluptas quas. Unde ducimus impedit nam, aperiam iste magnam ipsa dicta explicabo totam! Delectus dicta ad, similique eligendi quidem distinctio molestias.</p>
+										
+                                        </div>
+                                </div>
+                                <div class="col-md-12 col-lg-8">
+                                    <div class="row">
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Soutien</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="#" class="text-white">Forum</a></li>
+                                                    <li><a href="#" class="text-white">Termes et conditions</a></li>
+                                                    <li><a href="#" class="text-white">Question fréquemment posée</a></li>
+                                                    <li><a href="contact.php" class="text-white">Contact</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Liens rapides</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="about.php" class="text-white">À propos de nous</a></li>
+                                                    <li><a href="#" class="text-white">Propriété en vedette</a></li>
+                                                    <li><a href="#" class="text-white">Soumettre la propriété</a></li>
+                                                    <li><a href="agent.php" class="text-white">Nos agents</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Contactez-nous</h4>
+                                                <ul class="text-white">
+                                                    <li class="hover-text-primary"><i class="fas fa-map-marker-alt text-white mr-2 font-13 mt-1"></i>43010-sidi merouane, mila</li>
+                                                    <li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>(+213) 111-222-333</li>
+													<li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>(+213) 111-222-333</li>
+                                                    <li class="hover-text-primary"><i class="fas fa-envelope text-white mr-2 font-13 mt-1"></i>agence-immobiliere@gmail.com</li>
+                                                </ul>
+                                            </div>
+                                            <div class="footer-widget media-widget mt-4 text-white hover-text-primary"> <a href="#"><i class="fab fa-facebook-f"></i></a> <a href="#"><i class="fab fa-twitter"></i></a> <a href="#"><i class="fab fa-google-plus-g"></i></a> <a href="#"><i class="fab fa-linkedin-in"></i></a> <a href="#"><i class="fas fa-rss"></i></a> </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row copyright">
+                    <div class="col-sm-6"> <span class="text-white">© <?php echo date('Y');?> Agence Immobilere SiteWeb - Developer Par Medjkel Mouhammed</span> </div>
+                    <div class="col-sm-6">
+                        <ul class="line-menu text-white hover-text-primary float-right">
+                            <li><a href="#">Politique de confidentialité</a></li>
+                            <li>|</li>
+                            <li><a href="#"> Plan du site</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
---
+++ agence_immobiliaire/include/header.php
@@ -0,0 +1,71 @@
+<header id="header" class="transparent-header-modern fixed-header-bg-white w-100">
+            <div class="top-header bg-secondary">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <ul class="top-contact list-text-white  d-table">
+                                <li><a href="#"><i class="fas fa-phone-alt text-success mr-1"></i>+213 111-222-333</a></li>
+                                <li><a href="#"><i class="fas fa-envelope text-success mr-1"></i>agence-immobiliere@gmail.com</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="top-contact float-right">
+                                <ul class="list-text-white d-table">
+								<li><i class="fas fa-user text-success mr-1"></i>
+								<?php  if(isset($_SESSION['uemail']))
+								{ ?>
+								<a href="logout.php">Se déconnecter</a>&nbsp;&nbsp;<?php } else { ?>
+								<a href="login.php">Se connecter</a>&nbsp;&nbsp;
+								
+								| </li>
+								<li><i class="fas fa-user-plus text-success mr-1"></i><a href="register.php"> Registre</li><?php } ?>
+								</ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="main-nav secondary-nav hover-success-nav py-2">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <nav class="navbar navbar-expand-lg navbar-light p-0"> <a class="navbar-brand position-relative" href="index.php"><img class="nav-logo" src="images/logo/restatelg.png" alt=""></a>
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                    <ul class="navbar-nav mr-auto">
+                                        <li class="nav-item dropdown"> <a class="nav-link" href="index.php" role="button" aria-haspopup="true" aria-expanded="false">Accueille</a></li>
+										
+										<li class="nav-item"> <a class="nav-link" href="about.php">À propos</a> </li>
+										
+                                        <li class="nav-item"> <a class="nav-link" href="contact.php">Contact</a> </li>										
+										
+                                        <li class="nav-item"> <a class="nav-link" href="property.php">Immobilier</a> </li>
+                                        
+                                        <li class="nav-item"> <a class="nav-link" href="agent.php">Agent</a> </li>
+
+										
+										<?php  if(isset($_SESSION['uemail']))
+										{ ?>
+										<li class="nav-item dropdown">
+											<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Mon compte</a>
+											<ul class="dropdown-menu">
+												<li class="nav-item"> <a class="nav-link" href="profile.php">Profile</a> </li>
+												<li class="nav-item"> <a class="nav-link" href="feature.php">Votre immobilier</a> </li>
+												<li class="nav-item"> <a class="nav-link" href="logout.php">Se déconnecter</a> </li>	
+											</ul>
+                                        </li>
+										<?php } else { ?>
+										<li class="nav-item"> <a class="nav-link" href="login.php">Se connecter/Registre</a> </li>
+										<?php } ?>
+										
+                                    </ul>
+                                    
+									
+									<a class="btn btn-success d-none d-xl-block" style="border-radius:30px;" href="submitproperty.php">Ajouter Immobilier</a> 
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
